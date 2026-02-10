6a06da708cde9fce6e3df543e973aa16493d16d1
Robby Hutchison <136123318+Ybbor7456@users.noreply.github.com>
2026-02-09 23:29:11 -0500

    Implement logging functions in logger.c

--- drivers/logger.c
+++ drivers/logger.c
@@ -1,1 +1,22 @@
 // logger implementation 
+#include "drivers/logger.h"
+#include <stdarg.h>
+#include <stdio.h>    // if using vsnprintf
+
+uint32_t millis(void) {
+  return ticks;
+}
+
+void log_printf(const char *level, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    printf("[%8u] [%s] ", millis(), level);
+
+    // vprintf is the variadic version of printf
+    vprintf(fmt, args);
+    printf("\n");
+
+    va_end(args);
+}
+
