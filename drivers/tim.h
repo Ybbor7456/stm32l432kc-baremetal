025a9ad5455b9b28d98b0c84e4ebcf0ff91934d4
Robby <robby7456@gmail.com>
2026-04-22 23:28:24 -0400

    tim2 PWM init

--- drivers/tim.h
+++ drivers/tim.h
@@ -67,4 +67,34 @@ static inline void tim2_clear_uif(void){
     TIM2->SR &= ~BIT(0);
 }
 
+static inline void tim2_pwm_gpio_init(uint16_t pin){
+    gpio_set_mode(pin, GPIO_MODE_OUTPUT);
+    gpio_set_af(pin, 1);
+    gpio_set_otype(pin, 0);
+    gpio_set_select_speed(GPIO_VHIGH);
+    gpio_set_pupd(pin, NO_PULL); 
+}
+
+static inline void tim2_pwm_init(uint16_t pin, uint8_t ch_num){
+    tim2_pwm_gpio_init(pin);
+
+    if(ch_num == 1){
+        TIM2->CCRM1 &= ~(3u << 0); // CC1s = 00 output
+        TIM2->CCMR1 |= (1u << 11); // OC1PE
+        TIM2->CCMR1 &= ~(7u << 4); // clear OC1M
+        TIM2->CCMR1 |= (6u << 4); // PWM mode 1
+        TIM2->CCER |= (1u << 0);  // CC1E enable
+    }
+
+    if(ch_num ==2){
+        TIM2->CCRM1 &= ~(3u << 8); // CC2s = 00 output
+        TIM2->CCMR1 |= (1u << 11); // OC2PE
+        TIM2->CCMR1 &= ~(7u << 12); // clear OC2M
+        TIM2->CCMR1 |= (6u << 12); // PWM mode 1
+        TIM2->CCER |= (1u << 4);  // CC2E enable
+    }
+    TIM2->EGR |= BIT(0); // update event
+}
+
+
 // add ISR increments to be non-blocking
